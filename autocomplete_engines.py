06978b5051a31f34351629a10e454df3f41a5772
karkevin <46078220+karkevin@users.noreply.github.com>
2019-01-20 21:26:37 -0500

    added files via upload

---
+++ autocomplete_engines.py
@@ -0,0 +1,330 @@
+from __future__ import annotations
+import csv
+from typing import Any, Dict, List, Optional, Tuple
+
+from melody import Melody
+from prefix_tree import SimplePrefixTree, CompressedPrefixTree
+
+
+################################################################################
+# Text-based Autocomplete Engines (Task 4)
+################################################################################
+class LetterAutocompleteEngine:
+    """An autocomplete engine that suggests strings based on a few letters.
+
+    The *prefix sequence* for a string is the list of characters in the string.
+    This can include space characters.
+
+    This autocomplete engine only stores and suggests strings with lowercase
+    letters, numbers, and space characters; see the section on
+    "Text sanitization" on the assignment handout.
+
+    === Attributes ===
+    autocompleter: An Autocompleter used by this engine.
+    """
+    autocompleter: Autocompleter
+
+    def __init__(self, config: Dict[str, Any]) -> None:
+        """Initialize this engine with the given configuration.
+
+        <config> is a dictionary consisting of the following keys:
+            - 'file': the path to a text file
+            - 'autocompleter': either the string 'simple' or 'compressed',
+              specifying which subclass of Autocompleter to use.
+            - 'weight_type': either 'sum' or 'average', which specifies the
+              weight type for the prefix tree.
+
+        Each line of the specified file counts as one input string.
+        Note that the line may or may not contain spaces.
+        Each string must be sanitized, and if the resulting string contains
+        at least one alphanumeric character, it is inserted into the
+        Autocompleter.
+
+        *Skip lines that do not contain at least one alphanumeric character!*
+
+        When each string is inserted, it is given a weight of one.
+        Note that it is possible for the same string to appear on more than
+        one line of the input file; this would result in that string getting
+        a larger weight (because of how Autocompleter.insert works).
+        """
+        # We've opened the file for you here. You should iterate over the
+        # lines of the file and process them according to the description in
+        # this method's docstring.
+        if config["autocompleter"] == "simple":
+            self.autocompleter = SimplePrefixTree(config["weight_type"])
+        elif config["autocompleter"] == "compressed":
+            self.autocompleter = CompressedPrefixTree(config["weight_type"])
+
+        with open(config['file'], 'r', encoding='utf8') as file:
+            for line in file:
+                if line.isalnum():
+                    clean = line.lower()
+                    self.autocompleter.insert(clean, 1.0, list(clean))
+                else:
+                    clean = clean_string(line.lower())
+                    if clean != "":
+                        self.autocompleter.insert(clean, 1.0,
+                                                  list(clean))
+
+    def autocomplete(self, prefix: str,
+                     limit: Optional[int] = None) -> List[Tuple[str, float]]:
+        """Return up to <limit> matches for the given prefix string.
+
+        The return value is a list of tuples (string, weight), and must be
+        ordered in non-increasing weight. (You can decide how to break ties.)
+
+        If limit is None, return *every* match for the given prefix.
+
+        Note that the given prefix string must be transformed into a list
+        of letters before being passed to the Autocompleter.
+
+        Preconditions:
+            limit is None or limit > 0
+            <prefix> contains only lowercase alphanumeric characters and spaces
+        """
+        return self.autocompleter.autocomplete(list(prefix), limit)
+
+    def remove(self, prefix: str) -> None:
+        """Remove all strings that match the given prefix string.
+
+        Note that the given prefix string must be transformed into a list
+        of letters before being passed to the Autocompleter.
+
+        Precondition: <prefix> contains only lowercase alphanumeric characters
+                      and spaces.
+        """
+        self.autocompleter.remove(list(prefix))
+
+
+class SentenceAutocompleteEngine:
+    """An autocomplete engine that suggests strings based on a few words.
+
+    A *word* is a string containing only alphanumeric characters.
+    The *prefix sequence* for a string is the list of words in the string
+    (separated by whitespace). The words themselves do not contain spaces.
+
+    This autocomplete engine only stores and suggests strings with lowercase
+    letters, numbers, and space characters; see the section on
+    "Text sanitization" on the assignment handout.
+
+    === Attributes ===
+    autocompleter: An Autocompleter used by this engine.
+    """
+    autocompleter: Autocompleter
+
+    def __init__(self, config: Dict[str, Any]) -> None:
+        """Initialize this engine with the given configuration.
+
+        <config> is a dictionary consisting of the following keys:
+            - 'file': the path to a CSV file
+            - 'autocompleter': either the string 'simple' or 'compressed',
+              specifying which subclass of Autocompleter to use.
+            - 'weight_type': either 'sum' or 'average', which specifies the
+              weight type for the prefix tree.
+
+        Precondition:
+        The given file is a *CSV file* where each line has two entries:
+            - the first entry is a string
+            - the second entry is the a number representing the weight of that
+              string
+
+        Note that the line may or may not contain spaces.
+        Each string must be sanitized, and if the resulting string contains
+        at least one word, it is inserted into the Autocompleter.
+
+        *Skip lines that do not contain at least one alphanumeric character!*
+
+        When each string is inserted, it is given THE WEIGHT SPECIFIED ON THE
+        LINE FROM THE CSV FILE. (Updated Nov 19)
+        Note that it is possible for the same string to appear on more than
+        one line of the input file; this would result in that string getting
+        a larger weight.
+        """
+        # We haven't given you any starter code here! You should review how
+        # you processed CSV files on Assignment 1.
+        if config["autocompleter"] == "simple":
+            self.autocompleter = SimplePrefixTree(config["weight_type"])
+        elif config["autocompleter"] == "compressed":
+            self.autocompleter = CompressedPrefixTree(config["weight_type"])
+
+        with open(config['file'], encoding='utf8') as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                if line[0].isalnum():
+                    clean = line[0].lower()
+                    self.autocompleter.insert(clean, float(line[1]),
+                                              clean.split(" "))
+                else:
+                    clean = clean_string(line[0].lower())
+                    if clean != "":
+                        self.autocompleter.insert(clean, float(line[1]),
+                                                  clean.split(" "))
+
+    def autocomplete(self, prefix: str,
+                     limit: Optional[int] = None) -> List[Tuple[str, float]]:
+        """Return up to <limit> matches for the given prefix string.
+
+        The return value is a list of tuples (string, weight), and must be
+        ordered in non-increasing weight. (You can decide how to break ties.)
+
+        If limit is None, return *every* match for the given prefix.
+
+        Note that the given prefix string must be transformed into a list
+        of words before being passed to the Autocompleter.
+
+        Preconditions:
+            limit is None or limit > 0
+            <prefix> contains only lowercase alphanumeric characters and spaces
+        """
+        return self.autocompleter.autocomplete(prefix.split(" "), limit)
+
+    def remove(self, prefix: str) -> None:
+        """Remove all strings that match the given prefix.
+
+        Note that the given prefix string must be transformed into a list
+        of words before being passed to the Autocompleter.
+
+        Precondition: <prefix> contains only lowercase alphanumeric characters
+                      and spaces.
+        """
+        self.autocompleter.remove(prefix.split(" "))
+
+
+def clean_string(string: str) -> str:
+    """Returns the given string with only its alphanumerics and spaces."""
+    allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 "
+    return "".join(char if char in allowed else "" for char in string)
+
+
+################################################################################
+# Melody-based Autocomplete Engines (Task 5)
+################################################################################
+class MelodyAutocompleteEngine:
+    """An autocomplete engine that suggests melodies based on a few intervals.
+
+    The values stored are Melody objects, and the corresponding
+    prefix sequence for a Melody is its interval sequence.
+
+    Because the prefix is based only on interval sequence and not the
+    starting pitch or duration of the notes, it is possible for different
+    melodies to have the same prefix.
+
+    # === Private Attributes ===
+    autocompleter: An Autocompleter used by this engine.
+    """
+    autocompleter: Autocompleter
+
+    def __init__(self, config: Dict[str, Any]) -> None:
+        """Initialize this engine with the given configuration.
+
+        <config> is a dictionary consisting of the following keys:
+            - 'file': the path to a CSV file
+            - 'autocompleter': either the string 'simple' or 'compressed',
+              specifying which subclass of Autocompleter to use.
+            - 'weight_type': either 'sum' or 'average', which specifies the
+              weight type for the prefix tree.
+
+        Precondition:
+        The given file is a *CSV file* where each line has the following format:
+            - The first entry is the name of a melody (a string).
+            - The remaining entries are grouped into pairs (as in Assignment 1)
+              where the first number in each pair is a note pitch,
+              and the second number is the corresponding duration.
+
+            HOWEVER, there may be blank entries (stored as an empty string '');
+            as soon as you encounter a blank entry, stop processing this line
+            and move onto the next line the CSV file.
+
+        Each melody is be inserted into the Autocompleter with a weight of 1.
+        """
+        # We haven't given you any starter code here! You should review how
+        # you processed CSV files on Assignment 1.
+        if config["autocompleter"] == "simple":
+            self.autocompleter = SimplePrefixTree(config["weight_type"])
+        elif config["autocompleter"] == "compressed":
+            self.autocompleter = CompressedPrefixTree(config["weight_type"])
+
+        with open(config['file'], encoding='utf8') as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                if len(line) == 0:
+                    continue
+                name = line[0]
+                notes = [(int(line[i]), int(line[i + 1]))
+                         for i in range(1, len(line), 2)]
+                melody = Melody(name, notes)
+                sequence = [notes[i + 1][0] - notes[i][0]
+                            for i in range(0, len(notes) - 1)]
+                self.autocompleter.insert(melody, 1.0, sequence)
+
+    def autocomplete(self, prefix: List[int],
+                     limit: Optional[int] = None) -> List[Tuple[Melody, float]]:
+        """Return up to <limit> matches for the given interval sequence.
+
+        The return value is a list of tuples (melody, weight), and must be
+        ordered in non-increasing weight. (You can decide how to break ties.)
+
+        If limit is None, return *every* match for the given interval sequence.
+
+        Precondition:
+            limit is None or limit > 0
+        """
+        return self.autocompleter.autocomplete(prefix, limit)
+
+    def remove(self, prefix: List[int]) -> None:
+        """Remove all melodies that match the given interval sequence.
+        """
+        self.autocompleter.remove(prefix)
+
+
+###############################################################################
+# Sample runs
+###############################################################################
+def sample_letter_autocomplete() -> List[Tuple[str, float]]:
+    """A sample run of the letter autocomplete engine."""
+    engine = LetterAutocompleteEngine({
+        # NOTE: you should also try 'data/google_no_swears.txt' for the file.
+        'file': 'data/lotr.txt',
+        'autocompleter': 'simple',
+        'weight_type': 'sum'
+    })
+    return engine.autocomplete('frodo d', 20)
+
+
+def sample_sentence_autocomplete() -> List[Tuple[str, float]]:
+    """A sample run of the sentence autocomplete engine."""
+    engine = SentenceAutocompleteEngine({
+        'file': 'data/google_searches.csv',
+        'autocompleter': 'compressed',
+        'weight_type': 'sum'
+    })
+    return engine.autocomplete('how to', 20)
+
+
+def sample_melody_autocomplete() -> None:
+    """A sample run of the melody autocomplete engine."""
+    engine = MelodyAutocompleteEngine({
+        'file': 'data/random_melodies_c_scale.csv',
+        'autocompleter': 'compressed',
+        'weight_type': 'sum'
+    })
+    melodies = engine.autocomplete([2, 2], 20)
+    for melody, _ in melodies:
+        melody.play()
+
+
+if __name__ == '__main__':
+    import python_ta
+    python_ta.check_all(config={
+        'allowed-io': ['__init__'],
+        'extra-imports': ['csv', 'prefix_tree', 'melody']
+    })
+
+    # This is used to increase the recursion limit so that your sample runs
+    # work even for fairly tall simple prefix trees.
+    import sys
+    sys.setrecursionlimit(5000)
+
+    print(sample_letter_autocomplete())
+    # print(sample_sentence_autocomplete())
+    # sample_melody_autocomplete()
